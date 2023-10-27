a7a866b107d498429e080c98c2bf723d2049da99
Vanessa <lly219@gmail.com>
2023-10-27 11:34:53 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/9528

--- app/src/protyle/wysiwyg/index.ts
+++ app/src/protyle/wysiwyg/index.ts
@@ -453,6 +453,8 @@ export class WYSIWYG {
                         }
                     } else {
                         dragElement.parentElement.parentElement.style.width = (parseInt(dragElement.style.width) + 10) + "px";
+                        // 历史兼容
+                        dragElement.parentElement.parentElement.style.maxWidth = "";
                     }
                 };
 
